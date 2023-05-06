184b6db9ced1bb633f7184c2e3b2742d61e718ee
na7ux <ll-.-n47ux-.-ll@hotmail.com>
2023-05-06 14:57:20 -0300

    cambios en header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoApComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
-import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
@@ -14,22 +11,17 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoApComponent,
-    SocialComponent,
-    BannerComponent,
     AcercaDeComponent,
     ExperienceComponent,
     EducationComponent,
     SkillsComponent,
     ProjectsComponent,
     FooterComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
